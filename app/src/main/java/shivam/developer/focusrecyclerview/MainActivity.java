ec30220f47481b3db7569c9c3afe57e29e68337f
developers@techseria.com <developers@techseria.com>
2018-05-30 15:36:35 +0530

    Minor Change

--- app/src/main/java/shivam/developer/focusrecyclerview/MainActivity.java
+++ app/src/main/java/shivam/developer/focusrecyclerview/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void createDummyDataList() {
-        for (int i = 1; i <= 20; i++) {
+        for (int i = 1; i <= 25; i++) {
             dummyData.add("Item " + i);
         }
     }
